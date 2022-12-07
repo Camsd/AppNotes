0afedf6562bb57dc4c2b27bbb34814243cddafaa
Eduardost219 <eduard.ortega@boomlabs.edu.co>
2022-12-07 13:17:35 -0500

    componentes y estructura

--- src/App.js
+++ src/App.js
@@ -3,11 +3,52 @@ import './App.css';
 import Mensaje from './components/mensaje'
 
 function App() {
-  const [nombre, setNombre] = useState('cambio 1');
+  // const [nombre, setNombre] = useState('cambio 1');
+  const [items, setItems] = useState([{
+    id:0,
+    title: 'mi primer nota',
+    text: '# Hola a todos',
+    pinned: false,
+    created: Date.now()
+  },
+  {
+    id:0,
+    title: 'mi segunda nota',
+    text: '# Hola a todos',
+    pinned: false,
+    created: Date.now()
+  }]);
+
+  function handleClick(){
+    const note = {
+    id:2,
+    title: 'mi tercera nota nota',
+    text: '# Hola a todos',
+    pinned: false,
+    created: Date.now()
+    };
+    setItems([...items, note]);
+  }
+
   return(
-    <div>
-      <input type="text" onChange={ (e) => setNombre(e.target.value) } />
-      <Mensaje texto={nombre} />
+    <div className="App container">
+      <div className='panel'>
+        <div className='menu'>
+          <input className='search' placeholder='search....'></input>
+          <button className='btn' onClick={(e)=>handleClick()}>+ Nueva nota</button>
+        </div>
+        <div className='list'>
+            {
+              items.map((item, i)=>{
+                return <div key={item.id}>{item.title}</div>
+              })
+            }
+          </div>
+      </div>
+      <div>
+        <div className='editor'></div>
+        <div className='preview'></div>
+      </div>
     </div>
   );
   
